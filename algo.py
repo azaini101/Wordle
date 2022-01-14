bdd89e4859004234a7aa0e2b84d6edc4509ee22c
Ali Zaini <azaini@uno.edu>
2022-01-13 21:39:43 -0600

    Cleaned up code; working version

--- algo.py
+++ algo.py
@@ -1,36 +1,46 @@
 import random
 from termcolor import colored
-print(colored('not in word', 'red'))
-print(colored('correct position', 'green'))
-print(colored('letter in wrong position', 'yellow'))
-correct = "ggggg"
-word = ""
-with open("words.txt", "r") as file:
-    allText = file.read()
-    words = list(map(str, allText.split()))
-    # print random string
-    word = random.choice(words).upper()
-i = 1
-while True:
-    guess = input("Enter guess: ").upper()
-    if(guess == word):
-        break
-    i += 1
-    wordCopy = word
-    print(wordCopy)
-    while(len(guess) != 5):
-        guess = input("Enter guess: ").upper()
 
-    colors = ""
-    for i, n in enumerate(guess):
-        color = "red"
-        index = wordCopy.find(n)
-        if(index != -1):
-            color = "yellow"
-            if(i == index):
-                color = "green"
-            wordCopy = wordCopy.replace(n, ' ', 1)
-        colors += colored(n, color) + " "
-        
-    print(colors)
-print("Noice, you got it after {} tries.".format(i))
+def directions():
+    print(colored('not in word', 'red'))
+    print(colored('correct position', 'green'))
+    print(colored('letter in wrong position', 'yellow'))
+
+
+def getWords():
+    word = ""
+    with open("words.txt", "r") as file:
+        allText = file.read()
+        words = list(map(str, allText.split()))
+    return words
+correct = "ggggg"
+
+def main():
+    directions()
+    words = getWords()
+    word = random.choice(words).lower()
+    tries = 1
+    while True:
+        guess = input("Enter a guess: ").lower()
+        while(guess not in words):
+            guess = input("Enter a valid guess: ").lower()
+        if(guess == word):
+            break
+        tries += 1
+        wordCopy = word
+        colors = ""
+        for i, n in enumerate(guess):
+            color = "red"
+            index = wordCopy.find(n)
+            if(index != -1):
+                color = "yellow"
+                if(i == index):
+                    color = "green"
+                wordCopy = wordCopy.replace(n, ' ', 1)
+            colors += colored(n, color) + " "   
+        print(colors)
+    print(colored(" ".join(list(word)), 'green'))
+    print("Nice, you got it after {} tries.".format(tries))
+
+if __name__ == "__main__":
+    main()
